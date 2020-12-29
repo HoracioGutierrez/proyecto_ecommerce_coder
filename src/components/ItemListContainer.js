4efdb856a1304d4b973addd832b600447d797523
HoracioGutierrez <horacio.estevez@gmail.com>
2020-12-29 17:50:10 -0300

    Agrego cambios del desafio de clase 6

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react'
 import ItemCount from './ItemCount'
+import ItemList from './ItemList'
+
+const productos = [
+    {
+        id : "MLA832761839",
+        title : "Repetidor Tp-link Tl-wa850re Blanco 1 Unidad",
+        price : 2129,
+        pictureUrl : "https://http2.mlstatic.com/D_668117-MLA31786420833_082019-I.jpg"
+    },
+    {
+        id : "MLA871170293",
+        title : "Memoria Ram 8gb 1x8gb Kingston Hx426c16fb3/8 Hyperx Fury",
+        price : 4050,
+        pictureUrl : "https://http2.mlstatic.com/D_725124-MLA40927542006_022020-I.jpg"
+    },
+    {
+        id : "MLA883627045",
+        title : "Harry Potter Pack Colección 8 Peliculas En Dvd Saga Completa",
+        price : 2200,
+        pictureUrl : "https://http2.mlstatic.com/D_617506-MLA43746164726_102020-I.jpg"
+    },
+    {
+        id : "MLA749898152",
+        title : "Dragon Ball [coleccion Completa 2019] [53 Dvds]",
+        price : 7725,
+        pictureUrl : "https://http2.mlstatic.com/D_761041-MLA41995986461_052020-I.jpg"
+    }
+]
 
 const ItemListContainer = ({greeting}) => {
+
+    const [items,setItems] = useState([])
+
+    useEffect(()=>{
+        
+        let pedido = new Promise((res,rej)=>{
+            setTimeout(()=>{
+                res(productos)
+            },2000)
+        })
+
+        pedido
+        .then(res=>{
+            setItems(res)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+
+    },[])
+
     return (
         <div className="container">
             <h2>{greeting}</h2>
+            <ItemList items={items}/>
             <ItemCount stock={10} initial={1}/>
         </div>
     )
